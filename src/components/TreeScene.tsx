c644b81ef073f3b5547c58b45cab55d4822f9457
Brandyn Britton <brandynbb96@gmail.com>
2026-02-05 13:48:38 +0800

    switch assets

--- src/components/TreeScene.tsx
+++ src/components/TreeScene.tsx
@@ -5,9 +5,9 @@ import Tree from "./Tree";
 const TreeScene = () => {
     return (
         <Canvas camera={{ position: [-15, 15, 15], fov: 50 }} style={{ borderRadius: "2rem", width: '100%', height: '100%' }}>
-            <ambientLight intensity={0.5} />
+            <ambientLight intensity={0.8} />
             <directionalLight position={[10, 10, 5]} intensity={1} />
-            <Tree modelUrl="/willow_tree.glb" />
+            <Tree modelUrl="/willow_tree/willow_tree.gltf" />
             <OrbitControls enablePan enableZoom enableRotate />
         </Canvas>
     );
