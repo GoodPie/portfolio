c83414f09db049e1a15f4645e8b3afbaf038e8ec
Brandyn Britton <brandynbb96@gmail.com>
2026-02-05 12:43:14 +0800

    feat: added tree scene with Tree component

---
+++ src/components/TreeScene.tsx
@@ -0,0 +1,16 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Tree from "./Tree";
+
+const TreeScene = () => {
+    return (
+        <Canvas camera={{ position: [5, 5, 5], fov: 50 }} style={{ width: '100%', height: '100%' }}>
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[10, 10, 5]} intensity={1} />
+            <Tree modelUrl="/willow_tree.glb" />
+            <OrbitControls enablePan enableZoom enableRotate />
+        </Canvas>
+    );
+};
+
+export default TreeScene;
